cd83f9fec7bf278b59ce646befb0e059ef59efbb
wakaroquo <gabriel@hazera.net>
2025-02-28 18:02:07 +0100

    transformation fonctionnelle des fichiers en txt en parquet et en json a des fins de débuggage

---
+++ test_parquet.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+def get_spark() -> SparkSession:
+    return SparkSession.builder \
+        .appName("TxtToJsonFixedWidth") \
+        .config("spark.sql.files.maxPartitionBytes", "128MB") \
+        .config("spark.sql.adaptive.enabled", "true") \
+        .config("spark.sql.legacy.timeParserPolicy", "CORRECTED") \
+        .config("spark.sql.parquet.datetimeRebaseModeInWrite", "LEGACY") \
+        .getOrCreate()
+spark = get_spark()
+df_test = spark.read.parquet("output_parquet")
+df_test.show(10, truncate=False)
+df_test.printSchema()
+
+#Si tous ce passe bien, affiche le schéma parquet ainsi que les 10 premières entrées
--- txt_to_parquet.py
+++ txt_to_parquet.py
@@ -1,65 +1,80 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
-import json
-from pathlib import Path
 import os
-import death
 
+COLUMN_POSITIONS = [
+    ("nom_prenom", (0, 80)),
+    ("sexe", (80, 81)),
+    ("date_naissance", (81, 89)),
+    ("code_lieu_naissance", (89, 94)),
+    ("commune_naissance", (94, 124)),
+    ("pays_ou_lieu_naissance", (124, 154)),
+    ("date_deces", (154, 162)),
+    ("code_lieu_deces", (162, 167)),
+    ("num_acte_deces", (167, 176))
+]
 # Schéma attendu pour les données de décès comme précisé sur https://www.data.gouv.fr/fr/datasets/fichier-des-personnes-decedees/ (nécessité de le définir car fichier txt en non json)
-DEATH_RECORD_SCHEMA = StructType([
-    StructField("nom", StringType(), True),
-    StructField("prenoms", StringType(), True),
-    StructField("sexe", IntegerType(), True),
-    StructField("date_naissance", DateType(), True),
-    StructField("code_lieu_naissance", StringType(), True),
-    StructField("commune_naissance", StringType(), True),
-    StructField("pays_ou_lieu_naissance", StringType(), True),
-    StructField("date_deces", DateType(), True),
-    StructField("code_lieu_deces", StringType(), True),
-    StructField("num_acte_deces", StringType(), True)
+DEATH_SCHEMA = StructType([
+    StructField("nom_prenom", StringType()),
+    StructField("sexe", IntegerType()),
+    StructField("date_naissance", DateType()),
+    StructField("code_lieu_naissance", StringType()),
+    StructField("commune_naissance", StringType()),
+    StructField("pays_ou_lieu_naissance", StringType()),
+    StructField("date_deces", DateType()),
+    StructField("code_lieu_deces", StringType()),
+    StructField("num_acte_deces", StringType())
 ])
 
 def get_spark() -> SparkSession:
     return SparkSession.builder \
-        .appName("TxtToParquetFixedWidth") \
+        .appName("TxtToJsonFixedWidth") \
         .config("spark.sql.files.maxPartitionBytes", "128MB") \
         .config("spark.sql.adaptive.enabled", "true") \
-        .config("spark.sql.parquet.datetimeRebaseModeInWrite", "CORRECTED") \
-        .config("spark.eventLog.gcMetrics.youngGenerationGarbageCollectors", "G1 Young Generation") \
-        .config("spark.eventLog.gcMetrics.oldGenerationGarbageCollectors", "G1 Old Generation, G1 Concurrent GC") \
+        .config("spark.sql.legacy.timeParserPolicy", "CORRECTED") \
+        .config("spark.sql.parquet.datetimeRebaseModeInWrite", "LEGACY") \
         .getOrCreate()
-#j'ai essayé ce que les warning disaient pour le garbage collector, pas sur que ça change grand chose
-#la config datatime est nécessaire vu qu'il y a des dates avant 1900, lesquels ont une écriture différente selon le mode legacy ou hybride, cf doc sparks
 
-def parse_line(line : str) -> [death]:
-    # This should really return an option type but whatever, python…
-    try:
-        return death.death(line).__dict__.__str__()
-    except ValueError:
-        # Case where the line is not parsable, give up
-        return []
+def parse_fixed_width(df: DataFrame) -> DataFrame:
+    exprs = [
+        trim(substring(col("value"), pos[0] + 1, pos[1] - pos[0])).alias(name) 
+        for name, pos in COLUMN_POSITIONS
+    ]
+    
+    df_parsed = df.select(*exprs)
 
+    # Conversion du sexe en entier
+    df_parsed = df_parsed.withColumn("sexe", col("sexe").cast(IntegerType()))
 
-def first_read(data_file_paths: list[str]) -> DataFrame:
-    # Read all the files and parse everything that is parsable
-    data_file_paths=os.path.join(DATA_FOLDER,'*')
-    df = spark.read.text(data_file_paths)
-    return df.rdd.flatMap(parse_line)
+    # Conversion des dates avec gestion des valeurs inconnues
+    def clean_date(date_col):
+        return when(col(date_col).rlike("0000.*"), lit(None)) \
+               .otherwise(to_date(col(date_col), "yyyyMMdd"))
+
+    df_parsed = df_parsed.withColumn("date_naissance", clean_date("date_naissance")) \
+                         .withColumn("date_deces", clean_date("date_deces"))
+
+    return df_parsed
 
 #fonction main proche du tp3
 if __name__ == "__main__":
     spark = get_spark()
-    print("Version de Spark :", spark.version)
     DATA_FOLDER = "download"
     
-    parsed = first_read(DATA_FOLDER)
-
-    parsed.saveAsTextFile("data/parsed.json")
+    df = spark.read.text(os.path.join(DATA_FOLDER, "*.txt"))
+    parsed_df = parse_fixed_width(df)
+    #ecriture en json pour débugage
+    parsed_df.write \
+        .mode("overwrite") \
+        .option("dateFormat", "yyyy-MM-dd") \
+        .json("output_json")
+    #ecriture en parquet
+    parsed_df.write \
+        .mode("overwrite") \
+        .parquet("output_parquet")
 
     # This first read should also write a schema, for further
     # parsing, but it is merely the `death` class schema.
 
     # Todo: remove fileds of `death` that we do not plan on using
-    # Todo: load the json, and write it to parquet
-    

07e408db321aab1142f5f3c0931bfb9602ade155
wakaroquo <gabriel@hazera.net>
2025-03-04 09:24:55 +0100

    Ajout du nombre de données invalides (spoil y en a 0, ce qui ne résout pas l'incohérence pour certaines communes)

--- esperance.py
+++ esperance.py
@@ -17,7 +17,10 @@ if __name__ == "__main__":
     donnee = spark.read.parquet("output_parquet")
     donnee = donnee.filter(col("date_naissance").isNotNull() & col("date_deces").isNotNull())
     donnee = donnee.withColumn("age_deces", year(col("date_deces")) - year(col("date_naissance"))) 
+    donnee_invalide = donnee.filter(col("date_naissance").isNull() | col("date_deces").isNull())
+    nombre_individus_invalide = donnee_invalide.count()
     esperance = donnee.groupBy("code_lieu_naissance").agg(avg("age_deces").alias("esperance_vie"),count("*").alias("nombre_individus"))
+    print("Nombre d'individus avec une date de naissance ou de décès manquante :"+str(nombre_individus_invalide))
 
     # Pour faire des tops, on applique un filtre sql afin de ne pas avoir les communes avec moins de 4 individus décédés dans les données, souvent du à un ajout tardif de la commune dans les données
     esperance_filtre = esperance.filter(col("nombre_individus") > 10)
